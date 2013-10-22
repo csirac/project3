40b7e7cfecc618fe4184ab02d24a252af0267c23
Victoria Crawford <tori@alan-p6500z.(none)>
2013-10-22 15:17:06 -0400

    Project files

---
+++ Project3.scala
@@ -0,0 +1,77 @@
+import akka.actor._
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+import scala.math
+
+object Pastry { 
+
+  /**main method*/
+  def main(args: Array[String]) = { 
+    val b = 4 /**nodeid and keys are a sequence in base 2^b*/
+    val base = math.pow(2,b).toInt
+    val N = 100   /**number of nodes*/
+    val system = ActorSystem("PastrySystem")
+    var nodeArray = ArrayBuffer[ActorRef]()
+    var counter = 0
+    while(counter<N){ /**make nodes*/
+      var nodey = system.actorOf(Props(classOf[Node],genID(base)), counter.toString)
+      nodeArray = nodeArray += nodey
+      counter += 1
+    }
+    val a = genID(base)
+    println(a.toString)
+    val c = IDtoBigInt(a,base)
+    println(c)
+    println(BigInttoID(c,base).toString)
+    system.shutdown
+  }
+
+  /**nodes*/
+  class Node(id:ArrayBuffer[Int]) extends Actor {
+    var neighborhood = Set[ActorRef]()
+    def receive = {
+      case blah => {
+      }
+    }
+  }
+
+  /**generate random nodeid of length 32*/  
+  def genID(base:Int):ArrayBuffer[Int] = { 
+    val generator = new Random()
+    var nodeID = new ArrayBuffer[Int]()
+
+    var counter = 1
+    while(counter<=32){ 
+      nodeID = nodeID += (generator.nextInt(base.toInt))
+      counter += 1
+    } 
+    return nodeID
+  }
+
+  /**convert node id to BigInt*/
+  def IDtoBigInt(id:ArrayBuffer[Int], base:Int):BigInt = {
+    var myint:BigInt = 0
+    var index = 0
+    while(index < id.length){
+      var a = (BigInt(id(index)))*(BigInt(base).pow(index))
+      myint += a
+      index += 1
+    }
+    return myint
+  }
+
+  /**convert BigInt ID to sequence of length 32*/
+  def BigInttoID(id: BigInt, base:Int):ArrayBuffer[Int]={
+    var myArray = ArrayBuffer[Int]()
+    var counter = 0
+    var leftover = id
+    val bbase = BigInt(base)
+    while(counter<32){
+      myArray = myArray += (leftover.mod(bbase)).toInt
+      leftover = leftover/(bbase)
+      counter += 1
+    }
+    return myArray
+  }
+}
+
